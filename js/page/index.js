a49dbc63ee3b8cbb9491fba6220c496c804fa82f
hezhe <hezhe@ihangmei.com>
2017-01-20 19:07:52 +0800

    分离

--- js/page/index.js
+++ js/page/index.js
@@ -6,11 +6,12 @@ import dataFormat from 'dataFormat';
 import "babel-polyfill";
 
 const T_URL = 'http://139.217.29.222:6060/largeScreen/portalapp/getPortalTrainBusUser.action';
+const D_URL = 'http://139.217.29.222:6060/largeScreen/portalapp/getTrainBusYtjDevNum.action';
 const isMobile = /android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)|Symbian(OS)|Ubuntu Touch/i.test(window.navigator.userAgent); 
 
 
 window.addEventListener('DOMContentLoaded',function (){
-    var vEcharts = echarts.init(document.getElementById("v-echarts"));
+    var vEcharts;
 
     Vue.filter("format_time",function (str){
         return dataFormat(str,"YYYY-MM-DD hh:mm");
@@ -21,7 +22,8 @@ window.addEventListener('DOMContentLoaded',function (){
         data: {
             now: Date.now(),
             today_detail: {},
-            today_all: []
+            today_all: [],
+            device_all: []
         },
         watch: {
             today_detail: {
@@ -32,8 +34,12 @@ window.addEventListener('DOMContentLoaded',function (){
             }
         },
         mounted(){
-            vEcharts.showLoading();
-            this.get();
+            this.$nextTick(()=>{
+                vEcharts = echarts.init(document.getElementById("v-echarts"))
+                vEcharts.showLoading();
+                this.get();
+            });
+            
             setInterval(()=>{
                 this.now = Date.now();
                 this.get();
@@ -48,16 +54,14 @@ window.addEventListener('DOMContentLoaded',function (){
                             x_data: [],         // time
                             r_user: [],         // real_users
                             c_user: [],         // connected_users
-                            a_user: [],         // active_users
-                            d_user: []          // download
+                            a_user: []         // active_users
                         };
                         var today_all = [
                             {"type":"实际用户","trainUser":0,"busUser":0,"ytjUser":0},
                             {"type":"已连接用户","trainUser":0,"busUser":0,"ytjUser":0},
-                            {"type":"活跃用户","trainUser":0,"busUser":0,"ytjUser":0},
-                            {"type":"设备安装数","trainUser":0,"busUser":0,"ytjUser":0}
+                            {"type":"活跃用户","trainUser":0,"busUser":0,"ytjUser":0}
                         ];
-                        var iNow = arr.length/4*3;
+                        var iNow = arr.length;
                         for (var i = iNow-3; i < iNow; i++) {
                             switch(arr[i].userType){
                                 case 1: 
@@ -77,9 +81,6 @@ window.addEventListener('DOMContentLoaded',function (){
                                     break;
                             }
                         }
-                        today_all[3].trainUser=arr[arr.length-1].trainUser;
-                        today_all[3].busUser=arr[arr.length-1].busUser;
-                        today_all[3].ytjUser=arr[arr.length-1].ytjUser;
                         
                         const LAST_DAY = dataFormat(Date.now()-1000*3600*24,"YYYYMMDD");
                         for(let json of arr){
@@ -97,9 +98,6 @@ window.addEventListener('DOMContentLoaded',function (){
                                 case 3:
                                     today_detail.a_user.push(json.totalUser);
                                     break;
-                                case -1:
-                                    today_detail.d_user.push(json.totalUser);
-                                    break;
                             }
                         }
                         this.today_detail = today_detail;
@@ -108,9 +106,41 @@ window.addEventListener('DOMContentLoaded',function (){
                     .catch((e)=>{
                         console.error(`error ${e}`)
                     });
+                fetch(D_URL)
+                    .then(response=>response.json())
+                    .then(arr=>{
+                        var device_all = [];
+
+                        // 只取最后一条数据
+                        for (var i = arr.length-3; i < arr.length; i+=3) {
+                            var json = {
+                                "type": "设备安装数",
+                                "statDate": arr[i].statDate,
+                                "statHour": arr[i].statHour
+                            };
+                            for (var j = 0; j < 3; j++) {
+                                switch(arr[i+j].carType){
+                                    case 1:
+                                        json.trainUser = arr[i+j].sumOnline;
+                                        break;
+                                    case 2:
+                                        json.busUser = arr[i+j].sumOnline;
+                                        break;
+                                    case 11:
+                                        json.ytjUser = arr[i+j].sumOnline;
+                                        break;
+                                }
+                            }
+                            device_all.push(json);
+                        }
+                        this.device_all = device_all;
+                    })
+                    .catch((e)=>{
+                        console.error(`error ${e}`)
+                    });
             },
             updata_echarts: function (){
-                let {x_data,r_user,c_user,a_user,d_user} = this.today_detail;
+                let {x_data,r_user,c_user,a_user} = this.today_detail;
                 let formatter = isMobile?"":"{value}";
                 var option = {
                     title : {
@@ -164,16 +194,6 @@ window.addEventListener('DOMContentLoaded',function (){
                             name:'活跃用户',
                             type:'line',
                             data: a_user
-                            /*,markLine : {
-                                data : [
-                                    {type : 'average', name : '平均'}
-                                ]
-                            }*/
-                        },
-                        {
-                            name:'设备安装数',
-                            type:'line',
-                            data: d_user
                             /*,markLine : {
                                 data : [
                                     {type : 'average', name : '平均'}
