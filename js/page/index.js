4f08fbfccc3da2d3b7a7a8dd5bc91d3c93aa89b9
hezhe <hezhe@ihangmei.com>
2017-01-20 17:09:48 +0800

    vue computed

--- js/page/index.js
+++ js/page/index.js
@@ -48,31 +48,44 @@ window.addEventListener('DOMContentLoaded',function (){
                             x_data: [],         // time
                             r_user: [],         // real_users
                             c_user: [],         // connected_users
-                            a_user: []          // active_users
+                            a_user: [],         // active_users
+                            d_user: []          // download
                         };
                         var today_all = [
-                            {"type": "实际用户","trainUser":0,"busUser":0},
-                            {"type": "已连接用户","trainUser":0,"busUser":0},
-                            {"type": "活跃用户","trainUser":0,"busUser":0}
+                            {"type":"实际用户","trainUser":0,"busUser":0,"ytjUser":0},
+                            {"type":"已连接用户","trainUser":0,"busUser":0,"ytjUser":0},
+                            {"type":"活跃用户","trainUser":0,"busUser":0,"ytjUser":0},
+                            {"type":"设备安装数","trainUser":0,"busUser":0,"ytjUser":0}
                         ];
-                        for (var i = arr.length-3; i < arr.length; i++) {
+                        var iNow = arr.length/4*3;
+                        for (var i = iNow-3; i < iNow; i++) {
                             switch(arr[i].userType){
                                 case 1: 
                                     today_all[0].trainUser=arr[i].trainUser;
                                     today_all[0].busUser=arr[i].busUser;
+                                    today_all[0].ytjUser=arr[i].ytjUser;
                                     break;
                                 case 2: 
                                     today_all[1].trainUser=arr[i].trainUser;
                                     today_all[1].busUser=arr[i].busUser;
+                                    today_all[1].ytjUser=arr[i].ytjUser;
                                     break;
                                 case 3: 
                                     today_all[2].trainUser=arr[i].trainUser;
                                     today_all[2].busUser=arr[i].busUser;
+                                    today_all[2].ytjUser=arr[i].ytjUser;
                                     break;
                             }
                         }
-
+                        today_all[3].trainUser=arr[arr.length-1].trainUser;
+                        today_all[3].busUser=arr[arr.length-1].busUser;
+                        today_all[3].ytjUser=arr[arr.length-1].ytjUser;
+                        
+                        const LAST_DAY = dataFormat(Date.now()-1000*3600*24,"YYYYMMDD");
                         for(let json of arr){
+                            if (json.dayId==LAST_DAY) {
+                                json.timeId = '昨'+json.timeId;
+                            }
                             switch(json.userType){
                                 case 1:
                                     today_detail.x_data.push(json.timeId);
@@ -84,15 +97,20 @@ window.addEventListener('DOMContentLoaded',function (){
                                 case 3:
                                     today_detail.a_user.push(json.totalUser);
                                     break;
+                                case -1:
+                                    today_detail.d_user.push(json.totalUser);
+                                    break;
                             }
                         }
                         this.today_detail = today_detail;
                         this.today_all = today_all;
                     })
-                    .catch((e)=>console.error(`error ${e}`));
+                    .catch((e)=>{
+                        console.error(`error ${e}`)
+                    });
             },
             updata_echarts: function (){
-                let {x_data,r_user,c_user,a_user} = this.today_detail;
+                let {x_data,r_user,c_user,a_user,d_user} = this.today_detail;
                 let formatter = isMobile?"":"{value}";
                 var option = {
                     title : {
@@ -146,6 +164,16 @@ window.addEventListener('DOMContentLoaded',function (){
                             name:'活跃用户',
                             type:'line',
                             data: a_user
+                            /*,markLine : {
+                                data : [
+                                    {type : 'average', name : '平均'}
+                                ]
+                            }*/
+                        },
+                        {
+                            name:'设备安装数',
+                            type:'line',
+                            data: d_user
                             /*,markLine : {
                                 data : [
                                     {type : 'average', name : '平均'}
